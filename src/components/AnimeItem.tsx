a463914d52a77923df028d43f715e4d1114579e8
KevinStolk <stolkkevin02@gmail.com>
2023-02-02 14:38:13 +0100

    better legibillity

--- src/components/AnimeItem.tsx
+++ src/components/AnimeItem.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
-import { db } from '../db/firebase';
-import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
-import { FaRegTrashAlt } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../db/firebase";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 interface IAniList {
   id: number;
@@ -22,16 +22,16 @@ const AnimeItem = () => {
   const { user } = UserAuth();
 
   useEffect(() => {
-    onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
       setAnime(doc.data()?.animeList);
     });
   }, [user?.email]);
 
-  const aniPath = doc(db, 'users', `${user?.email}`);
+  const aniPath = doc(db, "users", `${user?.email}`);
 
   const deleteAnime = async (id: number): Promise<void> => {
     const confirmation = window.confirm(
-      'Are you sure you want to delete this anime?'
+      "Are you sure you want to delete this anime?"
     );
     if (!confirmation) {
       return;
@@ -47,14 +47,14 @@ const AnimeItem = () => {
     }
   };
 
-  const handleEpisodeChange = (id: number, action: 'add' | 'remove') => {
+  const handleEpisodeChange = (id: number, action: "add" | "remove") => {
     const selectedAnime = anime.find((ani) => ani.id === id);
     if (!selectedAnime) return;
 
     let updatedWatchedEpisodes = selectedAnime.watched_episodes;
-    if (action === 'add') {
+    if (action === "add") {
       updatedWatchedEpisodes += 1;
-    } else if (action === 'remove') {
+    } else if (action === "remove") {
       updatedWatchedEpisodes -= 1;
     }
 
@@ -97,68 +97,68 @@ const AnimeItem = () => {
           <tr
             className={`${
               ani.completed
-                ? 'bg-gray-300 h-[80px] border-b overflow-hidden'
-                : ''
+                ? "bg-gray-300 h-[80px] border-b overflow-hidden"
+                : ""
             } ${
               ani.completed
-                ? 'line-through h-[80px] border-b overflow-hidden'
-                : ''
+                ? "line-through h-[80px] border-b overflow-hidden"
+                : ""
             }`}
             key={ani.id}
           >
             <td>
               <input
-                type='checkbox'
+                type="checkbox"
                 checked={ani.completed}
                 onChange={() => toggleCompleted(ani.id)}
               />
             </td>
             <td>{index + 1}</td>
             <td>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <img
-                  className='w-16 mr-2 pb-2 hover:cursor-pointer'
+                  className="w-16 mr-2 pb-2 hover:cursor-pointer"
                   onClick={() => {
-                    window.open(`${ani?.url}`, '_blank');
+                    window.open(`${ani?.url}`, "_blank");
                   }}
                   src={ani?.image}
                   alt={ani?.image}
                 />
-                <p className='hidden sm:table-cell'>{ani.title}</p>
+                <p className="hidden sm:table-cell">{ani.title}</p>
               </div>
             </td>
             <td></td>
             <td></td>
             <td></td>
             <td>{parseInt(ani.score)}</td>
-            <td className='w-[180px] hidden md:table-cell'>{ani.type}</td>
-            <td className='w-[180px] hidden sm:table-cell'>
+            <td className="w-[180px] hidden md:table-cell">{ani.type}</td>
+            <td className="w-[180px] hidden sm:table-cell">
               {ani.watched_episodes > 0 ? (
                 <button
-                  onClick={() => handleEpisodeChange(ani.id, 'remove')}
-                  className='hover:bg-right hover:text-black appearance-none outline-none bg-none border-none cursor-pointer  p-2 mr-2 rounded text-black  text-sm text-bold uppercase transition'
+                  onClick={() => handleEpisodeChange(ani.id, "remove")}
+                  className="bg-button text-white cursor-pointer p-2 mr-2 rounded text-sm text-bold uppercase transition"
                 >
                   -
                 </button>
               ) : (
-                ''
+                ""
               )}
-              {ani.watched_episodes} / {ani.episodes}{' '}
+              {ani.watched_episodes} / {ani.episodes}{" "}
               {ani.episodes !== ani.watched_episodes ? (
                 <button
-                  onClick={() => handleEpisodeChange(ani.id, 'add')}
-                  className='hover:bg-right hover:text-black appearance-none outline-none bg-none border-none cursor-pointer  p-2 mr-2 rounded text-black  text-sm text-bold uppercase transition'
+                  onClick={() => handleEpisodeChange(ani.id, "add")}
+                  className="bg-button text-white cursor-pointer  p-2 mr-2 rounded text-sm text-bold uppercase transition"
                 >
                   +
                 </button>
               ) : (
-                ''
+                ""
               )}
             </td>
-            <td className='pl-8'>
+            <td className="pl-8">
               <FaRegTrashAlt
                 onClick={() => deleteAnime(ani?.id)}
-                className='cursor-pointer hover:text-red-500 transition'
+                className="cursor-pointer hover:text-red-500 transition"
               />
             </td>
           </tr>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import ThemeToggle from './ThemeToggle';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import { UserAuth } from '../context/AuthContext';
-import MALLogoBlack from '../assets/img/logo_mal_black.png';
-import MALLogoWhite from '../assets/img/logo_mal.png';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { UserAuth } from "../context/AuthContext";
+import MALLogoBlack from "../assets/img/logo_mal_black.png";
+import MALLogoWhite from "../assets/img/logo_mal.png";
 
 const Navbar = () => {
   const { user, logout } = UserAuth();
@@ -15,7 +15,7 @@ const Navbar = () => {
   const handleSignOut = async () => {
     try {
       await logout();
-      navigate('/');
+      navigate("/");
       setNav(!nav);
     } catch (err) {
       console.error(err);
@@ -27,77 +27,77 @@ const Navbar = () => {
   };
 
   return (
-    <div className='bg-[#2f51a3] w-full px-2 flex items-center justify-between h-[58px] font-bold'>
-      <div className='logo mx-2'>
-        <Link to='/'>
+    <div className="bg-[#2f51a3] w-full px-2 flex items-center justify-between h-[58px] font-bold">
+      <div className="logo mx-2">
+        <Link to="/">
           <img
-            className='flex w-32 dark:hidden'
+            className="flex w-32 dark:hidden"
             src={MALLogoBlack}
             alt={MALLogoBlack}
           />
           <img
-            className='hidden w-32 dark:flex'
+            className="hidden w-32 dark:flex"
             src={MALLogoWhite}
             alt={MALLogoWhite}
           />
         </Link>
       </div>
       {user ? (
-        <div className='hidden md:block'>
-          <small className='text-primary'>Welcome {user?.email}</small>
+        <div className="hidden md:block">
+          <small className="text-white font-bold">Welcome {user?.email}</small>
 
           <button
             onClick={handleSignOut}
-            className='bg-button text-btnText px-5 py-2 ml-2 rounded-md shadow-lg hover:shadow-2xl'
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-md shadow-lg hover:shadow-2xl"
           >
             Sign out
           </button>
         </div>
       ) : (
-        <div className='hidden md:block'>
-          <Link to='/signin' className='p-4 hover:text-accent transition'>
+        <div className="hidden md:block">
+          <Link to="/signin" className="p-4 hover:text-accent transition">
             Sign In
           </Link>
           <Link
-            to='/signup'
-            className='bg-button text-btnText px-5 py-2 ml-2 rounded-md shadow-lg hover:shadow-2xl'
+            to="/signup"
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-md shadow-lg hover:shadow-2xl"
           >
             Sign Up
           </Link>
         </div>
       )}
-      <div onClick={handleNav} className='block md:hidden cursor-pointer z-10'>
+      <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={20} />}
       </div>
       {/* Mobile Menu  */}
       <div
         className={
           nav
-            ? 'md:hidden fixed left-0 top-16 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-500 z-10'
-            : 'fixed top-[-100%] h-[90%] flex flex-col items-center justify-between'
+            ? "md:hidden fixed left-0 top-16 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-500 z-10"
+            : "fixed top-[-100%] h-[90%] flex flex-col items-center justify-between"
         }
       >
-        <ul className='w-full list-none p-4'>
-          <li onClick={handleNav} className='border-b py-6'>
-            <Link to='/'>Home</Link>
+        <ul className="w-full list-none p-4">
+          <li onClick={handleNav} className="border-b py-6">
+            <Link to="/">Home</Link>
           </li>
-          <li className='py-6'>
+          <li className="py-6">
             <ThemeToggle />
           </li>
         </ul>
         {!user ? (
-          <div className='flex flex-col w-full p-4'>
-            <Link to='/signup'>
+          <div className="flex flex-col w-full p-4">
+            <Link to="/signup">
               <button
                 onClick={handleNav}
-                className='hover:cursor-pointer w-full m-y2 p-3 bg-button text-btnText rounded-md shadow-xl'
+                className="hover:cursor-pointer w-full m-y2 p-3 bg-button text-btnText rounded-md shadow-xl"
               >
                 Sign up
               </button>
-              <Link to='/signin'>
+              <Link to="/signin">
                 <button
                   onClick={handleNav}
-                  className='hover:cursor-pointer w-full my-2 p-3 border-blue-500 text-accent  border-2 border-secondary rounded-md shadow-xl hover:border-none'
+                  className="hover:cursor-pointer w-full my-2 p-3 border-blue-500 text-accent  border-2 border-secondary rounded-md shadow-xl hover:border-none"
                 >
                   Sign In
                 </button>
@@ -107,7 +107,7 @@ const Navbar = () => {
         ) : (
           <button
             onClick={handleSignOut}
-            className='w-full m-y2 p-3 bg-button text-btnText rounded-md shadow-xl'
+            className="w-full m-y2 p-3 bg-button text-btnText rounded-md shadow-xl"
           >
             Sign Out
           </button>
